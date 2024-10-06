b13be9e2ef01eb68de6d05bfbf08f39fa6fc4f60
Farrux20030308 <kurieito0001@gmail.com>
2024-10-06 18:51:16 +0500

    Homework start

---
+++ script.js
@@ -0,0 +1,22 @@
+alert('Добро пожаловать!')
+let name = prompt('Введите ваше имя: ');
+let age = prompt('Введите ваш возраст: ')
+let plus = prompt('Пример 1: 46+54= ')
+let minus =  prompt('Пример 2: 328-56= ')
+let multi = prompt('Пример 3: 24*5= ')
+let division = prompt('Пример 4: 168/2= ')
+let divisionWithRemainder =  prompt('Пример5: 215/2= ')
+alert("Пожалуйсте зайдите в консоль разработчика!")
+
+console.log("Ваше имя: " + name + ".");
+console.log("Ваш возраст: " + age + ".");
+console.log("Ваш ответ на 1 пример: 46+54=" + plus + ". Правильный ответ: 100");
+console.log("Ваш ответ на 2 пример: 328-56= " + minus  + ". Правильный ответ: 272");
+console.log("Ваш ответ на 3 пример: 24*5=" + multi + ". Правильный ответ: 120");
+console.log("Ваш ответ на 4 пример: 168/2=" + division + ". Правильный ответ: 84");
+console.log("Ваш ответ на 5 пример: 215/2=" + divisionWithRemainder  + ". Правильный ответ: 107 остаток 1");
+
+
+
+
+
